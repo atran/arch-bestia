1d02b2d8bf6c1d3df8e959db94584c49aa42d580
anthony tran <atran.dummy@gmail.com>
2021-03-13 18:16:31 -0500

    init code defaults

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,27 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import useSWR from 'swr';
+
 export default function Home() {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data } = useSWR('/api/readfiles', fetcher);
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>The Architectural Beast</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header />
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>Grid images read from directory:</h1>
+          <div className="grid">
+            {!data && "Loading..."}
+            {data && data.map(imgPath => <figure><img src={imgPath} alt=""/></figure>)}
+          </div>
       </main>
 
       <Footer />
