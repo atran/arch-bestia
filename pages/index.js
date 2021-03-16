1113e452631a12074aa9a430c4ed3f32f3f4222f
anthony tran <atran.dummy@gmail.com>
2021-03-16 14:11:23 -0400

    add some movement

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,83 @@
+import padStart from 'lodash/padStart';
+import classnames from 'classnames';
+
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React from 'react';
 
-import useSWR from 'swr';
 
-export default function Home() {
-  const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data } = useSWR('/api/readfiles', fetcher);
+class Home extends React.Component {
+  state = {
+    active: false,
+    initialOne: false,
+    initialTwo: false,
+  }
 
-  return (
-    <div className="container">
-      <Head>
-        <title>The Architectural Beast</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <Header />
+  setActiveState = () => {
+    this.setState({
+      active: true,
+    })
+    setTimeout(() => {
+      this.setState({ initialTwo: true })
+    }, 10);
+  }
+  
+  createGrid() {
+    let gridEls = []
+    for (let x = 0; x <= 15; x++) {
+      for (let y = 0; y <= 15; y++) {
+        const directory = 'iterations/2021-03-15/'
+        const filename = `out_${padStart(x, 2, '0')}_${padStart(y, 2, '0')}.png`
+        gridEls.push(
+          <figure>
+            <img src={`${directory}${filename}`} alt="" onClick={this.addGrid}/>
+          </figure>
+        )
+      }
+    }
+    return gridEls;
+  }
 
-      <main>
-        <div className="grid">
-          {!data && "Loading..."}
-          {data && data.map(imgPath => <figure><img src={imgPath} alt=""/></figure>)}
-        </div>
-      </main>
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ initialOne: true })
+    }, 100);
+  }
 
-      <Footer />
-    </div>
-  )
+  render() {
+    const { active, initialOne, initialTwo } = this.state;
+    const firstGridClass = classnames('grid', {
+      moved: active,
+      initial: initialOne
+    });
+    const secondGridClass = classnames('grid', {
+      initial: initialTwo,
+    });
+
+    return (
+      <div className="container">
+        <Head>
+          <title>The Architectural Beast</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        <Header />
+
+        <main ref={this.mainElRef}>
+          <div className={firstGridClass} onClick={this.setActiveState}>
+            {this.createGrid()}
+          </div>
+          {
+            active && <div className={secondGridClass}>{this.createGrid()}</div>
+          }
+
+        </main>
+
+        <Footer />
+      </div>
+    )
+  }
 }
+
+export default Home;
