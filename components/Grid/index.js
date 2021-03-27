1441f2da30beada97a488e11137a98ebe0d1140d
anthony tran <atran.dummy@gmail.com>
2021-03-27 15:45:29 -0400

    use a <Grid /> component

--- components/Grid/index.js
+++ components/Grid/index.js
@@ -4,26 +4,89 @@ import classnames from 'classnames';
 
 import Item from '@components/Item';
 
-export default class Grid extends React.Component {
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mouseX: -1,
+      mouseY: -1,
+      windowWidth: -1,
+      windowHeight: -1,
+    }
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ 
+      windowWidth: window.innerWidth, 
+      windowHeight: window.innerHeight 
+    });
+  }
+
+  changeCaption(x, y) {
+    const loc = (x * 15) + y;
+    console.log(x, y);
+  }
+
   createGrid() {
     let gridEls = []
     for (let x = 0; x <= 15; x++) {
       for (let y = 0; y <= 15; y++) {
         const directory = 'iterations/2021-03-16/'
         const filename = `out256_${padStart(y, 2, '0')}_${padStart(x, 2, '0')}.png`
-        gridEls.push(<Item imgSrc={`${directory}${filename}`} />)
+        gridEls.push(          
+          <Item 
+            imgSrc={`${directory}${filename}`} 
+            mouseEnterHandler={() => this.changeCaption(x, y)}
+          />)
       }
     }
     return gridEls;
   }
 
+  componentDidMount() {
+    const { isPast } = this.props;
+    if (!isPast) {
+      document.addEventListener('mousemove', (e) => {
+        this.setState({ mouseX: e.pageX, mouseY: e.pageY });
+      });
+
+      window.addEventListener('resize', this.updateWindowDimensions);
+      this.updateWindowDimensions();
+    }
+  }
+
   render() {
-    const gridClasses = classnames('grid');
+    const {
+      windowWidth,
+      windowHeight,
+      mouseX,
+      mouseY
+    } = this.state;
+    const { isPast } = this.props;
+
+    const gridXOffset = mouseX - (windowWidth / 2);
+    const gridYOffset = mouseY - (windowHeight / 2);
+    const gridTransform = `${gridXOffset / -10}px, ${gridYOffset / -10}px`;
+
+    const gridClasses = classnames('grid', {
+      'previous-grid': isPast
+    });
+    const gridStyle = isPast ? null
+    : {
+      transform: `translate(${gridTransform})`
+    }
+    ;
     
     return (
-      <div className={gridClasses}>
-      { this.createGrid() }
+      <div 
+        className={gridClasses}
+        style={gridStyle}
+      >
+        { this.createGrid() }
       </div>
     )
   }
 }
+
+export default Grid;
--- pages/index.js
+++ pages/index.js
@@ -11,81 +11,7 @@ import Item from '@components/Item';
 import CAPTIONS from './../constants/captions'
 
 class Home extends React.Component {
-  state = {
-    active: true,
-    initialOne: false,
-    initialTwo: true,
-    mouseX: -1,
-    mouseY: -1,
-    windowWidth: -1,
-    windowHeight: -1,
-    caption: CAPTIONS[0]
-  }
-
-  updateWindowDimensions = () => {
-    this.setState({ windowWidth: window.innerWidth, windowHeight: window.innerHeight });
-  }
-
-  setActiveState = () => {
-    this.setState({
-      active: true,
-    })
-    setTimeout(() => {
-      this.setState({ initialTwo: true })
-    }, 10);
-  }
-
-  changeCaption(index) {
-    this.setState({
-      caption: CAPTIONS[index]
-    })
-  }
-
-  createGrid() {
-    let gridEls = []
-    for (let x = 0; x <= 18; x++) {
-      for (let y = 0; y <= 18; y++) {
-        const directory = 'iterations/2021-03-16/'
-        const filename = `out256_${padStart(y, 2, '0')}_${padStart(x, 2, '0')}.png`
-        gridEls.push(
-          <Item 
-            imgSrc={`${directory}${filename}`} 
-            mouseEnterHandler={() => this.changeCaption((x * 15) + y)}
-          />
-        )
-      }
-    }
-    return gridEls;
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ initialOne: true })
-    }, 100);
-
-    document.addEventListener('mousemove', (e) => {
-      this.setState({mouseX: e.pageX, mouseY: e.pageY});
-    });
-
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
   render() {
-    const { active, initialOne, initialTwo, mouseX, mouseY, windowWidth, windowHeight, caption } = this.state;
-
-    const firstGridClass = classnames('grid', {
-      moved: active,
-      initial: initialOne
-    });
-    const secondGridClass = classnames('grid', {
-      initial: initialTwo,
-    });
-
-    const gridXOffset = mouseX - (windowWidth / 2);
-    const gridYOffset = mouseY - (windowHeight / 2);
-    const gridTransform = `${gridXOffset / -10}px, ${gridYOffset / -10}px`;
-
     return (
       <div className="container">
         <Head>
@@ -95,36 +21,18 @@ class Home extends React.Component {
         
         <Header />
 
-        <main ref={this.mainElRef}>
-          {/* <div className="grids">
-          </div> */}
-
-          <div 
-            className={firstGridClass}
-          >
-            {this.createGrid()}
-          </div>
-          {
-            active 
-            && 
-            <div 
-              className={secondGridClass}
-              style={{
-                transform: `translate(${gridTransform})`
-              }}
-            >
-              {this.createGrid()}
-            </div>
-          }
-          {/* <div className="breadcrumbs" onClick={this.setActiveState}>
-            <span className="arrow">
-              {String.fromCharCode(11105)}
-            </span>
-            <div>Go to previous day</div>
-          </div> */}
+        <main className="grids" ref={this.mainElRef}>
+          <Grid isPast={true} />
+          <Grid />
         </main>
 
-        <Footer caption={caption} />
+        {/* <div className="breadcrumbs" onClick={this.setActiveState}>
+          <span className="arrow">
+            {String.fromCharCode(11105)}
+          </span>
+          <div>Go to previous day</div>
+        </div> */}
+        {/* <Footer caption={caption} /> */}
       </div>
     )
   }
