f5ff76dcd3d6d8ab1282687f5a9aa065d114fcf4
anthony tran <atran.dummy@gmail.com>
2021-03-31 17:46:02 -0400

    add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { AnimateSharedLayout } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Head from 'next/head';
 
 import Footer from '@components/Footer';
@@ -6,17 +6,25 @@ import Footer from '@components/Footer';
 import '@styles/reset.css';
 import '@styles/globals.css'
 
-function Application({ Component, pageProps }) {
+function Application({ Component, pageProps, router }) {
   return (
     <>
     <Head>
       <title>The Architectural Beast</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <AnimateSharedLayout>
-      <Component {...pageProps} />
+    <AnimatePresence>
+      <motion.div 
+        key={router.route}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        exitBeforeEnter
+      >
+        <Component {...pageProps} />
+      </motion.div>
       <Footer />
-    </AnimateSharedLayout>
+    </AnimatePresence>
     </>
   )
 }
