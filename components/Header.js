e8484e8e8dd58348e4e6cbbb53a3a2705ae0a6d7
anthony tran <atran.dummy@gmail.com>
2021-03-31 18:10:34 -0400

    some logic for iteration nav

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
-import { format } from 'date-fns'
+import { format, parse } from 'date-fns'
 
 import styles from './Header.module.css'
 
-export default function Header({currDate, changeDate}) {
+export default function Header({
+  firstDateFormatted, 
+  lastDateFormatted, 
+  currDate, 
+  goToPrevDay, 
+  goToNextDay
+}) {
   const currDateFormatted = format(currDate, 'yyyy-MM-dd');
   const directory = `iterations/${currDateFormatted}`;
   const iterationVideoSrc = `${directory}/bounce.mp4`;
   
+  // TODO: it has to be a <> comparison
+  const hasPrevDays = lastDateFormatted === currDateFormatted;
+  const hasNextDays = firstDateFormatted === currDateFormatted;
+
   return (
     <>
       <header className={styles.header}>
@@ -16,18 +26,24 @@ export default function Header({currDate, changeDate}) {
         </h1>
 
         <div className={styles.breadcrumbs}>
-          <div className={styles.breadcrumb} onClick={changeDate}>
-            <span className={styles.arrow}>
-              {String.fromCharCode(11105)}
-            </span>
-            <p>Prev Day</p>
-          </div>
-          <div className={styles.breadcrumb} onClick={changeDate}>
-            <span className={styles.arrow}>
-              {String.fromCharCode(11107)}
-            </span>
-            <p>Next Day</p>
-          </div>
+          {
+            hasPrevDays &&
+            <div className={styles.breadcrumb} onClick={goToPrevDay}>
+              <span className={styles.arrow}>
+                {String.fromCharCode(11105)}
+              </span>
+              <p>Prev Day</p>
+            </div>
+          }
+          {
+            hasNextDays &&
+            <div className={styles.breadcrumb} onClick={goToNextDay}>
+              <span className={styles.arrow}>
+                {String.fromCharCode(11107)}
+              </span>
+              <p>Next Day</p>
+            </div>
+          }
         </div>
       </header>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { AnimatePresence } from "framer-motion";
-import { parse, subDays, isBefore } from 'date-fns'
+import { parse, format, subDays, addDays, isBefore } from 'date-fns'
 
 import Header from '@components/Header';
 import Grid from '@components/Grid';
 
-const CURRENT_DATE = '2021-03-30';
+const FIRST_DATE = '2021-03-29';
+const LAST_DATE = '2021-03-30';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     
-    const currDate = parse(CURRENT_DATE, 'yyyy-MM-dd', new Date());
+    const currDate = parse(LAST_DATE, 'yyyy-MM-dd', new Date());
     const prevDate = subDays(currDate, 1);
     this.state = {
       caption: '',
@@ -25,11 +26,28 @@ class Home extends React.Component {
     });
   }
 
-  changeDate = () => {
+  goToPrevDay = () => {
     const { iterations } = this.state;
 
     const prevDate = subDays(iterations[0], 1);
     const currDate = subDays(iterations[1], 1);
+    const nextIterations = [prevDate, currDate];
+
+    // const currDateFormatted = format(prevDate, 'yyyy-MM-dd');
+    // if (currDateFormatted === FIRST_DATE) {
+    //   nextIterations.shift();
+    // }
+    
+    this.setState({
+      iterations: nextIterations,
+    });
+  }
+
+  goToNextDay = () => {
+    const { iterations } = this.state;
+
+    const prevDate = addDays(iterations[0], 1);
+    const currDate = addDays(iterations[1], 1);
     this.setState({
       iterations: [prevDate, currDate]
     });
@@ -44,8 +62,11 @@ class Home extends React.Component {
     return (
       <div className="container">
         <Header 
+          firstDateFormatted={FIRST_DATE}
+          lastDateFormatted={LAST_DATE}
           currDate={iterations[1]} 
-          changeDate={this.changeDate}
+          goToPrevDay={this.goToPrevDay}
+          goToNextDay={this.goToNextDay}
         />
 
         <main className="grids">
