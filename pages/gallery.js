d39f4691426d4568c8df9da5bf33d1fc552be940
anthony tran <atran.dummy@gmail.com>
2021-04-01 14:59:16 -0400

    static file loc

--- pages/gallery.js
+++ pages/gallery.js
@@ -41,33 +41,33 @@ function Gallery({ artists }) {
             y: offsets[1],
           }}
         >
-          {/* {
+          {
             artists.map(({ artist, link, directory}) => (
               <Artist key={directory} name={artist} link={link} dir={directory} />
             ))
-          } */}
+          }
         </motion.div>
       </main>
     </div>
   )
 }
 
-// export async function getStaticProps() {
-//   const dev = process.env.NODE_ENV !== 'production';
-//   const server = dev ? 'http://localhost:3000' : 'https://archbestia.com';
+export async function getStaticProps() {
+  const dev = process.env.NODE_ENV !== 'production';
+  const server = dev ? 'http://localhost:3000' : 'https://archbestia.com';
 
-//   const res = await fetch(`${server}/artists/index.json`)
-//   const artists = await res.json()
+  const res = await fetch(`${server}/artists/index.json`)
+  const artists = await res.json()
 
-//   if (!artists) {
-//     return {
-//       notFound: true,
-//     }
-//   }
+  if (!artists) {
+    return {
+      notFound: true,
+    }
+  }
 
-//   return {
-//     props: { artists }, // will be passed to the page component as props
-//   }
-// }
+  return {
+    props: { artists }, // will be passed to the page component as props
+  }
+}
 
 export default Gallery;
